daa881b6f6439b25a81f105fd4c2fd40a3e5717c
Dieferson Romanoski <diefe-ki@outlook.com>
2021-03-30 23:02:15 -0300

    dashboard screen created

--- pandemicweb/lib/pages/home_page.dart
+++ pandemicweb/lib/pages/home_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pandemicweb/components/body.component.dart';
+import 'package:pandemicweb/components/custom_nav_bar.dart';
+import 'package:pandemicweb/utils/colors.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color(0xFF2f3438),
+        color: ColorsModel.secundaryColor,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomNavBar(),
+            Body(),
+          ],
+        ),
       ),
     );
   }
