7109900aa41ce8718ac3d4980b4a5a5ecdfd2bb8
André Luis Moreira <andreluismoreirasmo@gmail.com>
2021-03-31 20:06:53 -0300

    Remove flutter

--- frontend/lib/utils/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorsModel {
-  static const Color primaryColor = Color(0xFF2f3438);
-  static const Color secundaryColor = Color(0xFF222529);
-  static const Color textColor = Color(0xFFFFFFFF);
-}
